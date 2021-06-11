b56ebae01bff91f6130cd822ef57cc37ddecee5a
Simar2906 <simar2906@gmail.com>
2021-06-12 04:15:31 +0530

    working on jumps

--- Game Jam/Assets/Scripts/Player_Movement.cs
+++ Game Jam/Assets/Scripts/Player_Movement.cs
@@ -9,27 +9,70 @@ public class Player_Movement : MonoBehaviour
     private bool facingRight = true;
     private float moveDirection;
     public float moveSpeed;
+    public Animator animator;
 
+    public KeyCode jumpKey = KeyCode.W;
+
+    public float jumpForce;
+    private bool isJumping = false;
     private void Awake() 
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveDirection = Input.GetAxis("Horizontal"); //(-1 to +1)
+        // change velocity variables and animation speed
+        ProcessMethod();
+        // change sprite direction
+        DirectionChange();
+        //change animation
+        Animate();
 
+    }
+
+    private void FixedUpdate() 
+    {
+        //move
+        Move();
+    }
+    private void Move()
+    {
         rigidBody.velocity = new Vector2(moveDirection * moveSpeed, rigidBody.velocity.y);
+        if(isJumping)
+        {
+            rigidBody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
 
-        if(moveDirection > 0 && !facingRight)
+    private void Animate()
+    {
+        animator.SetFloat("speed", Mathf.Abs(moveDirection));
+    }
+
+    private void DirectionChange()
+    {
+        if (moveDirection > 0 && !facingRight)
         {
             FlipCharacter();
         }
-        else if(moveDirection < 0 && facingRight)
+        else if (moveDirection < 0 && facingRight)
         {
             FlipCharacter();
         }
+    }
+
+    private void ProcessMethod()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); //(-1 to +1)
+        if(Input.GetKeyDown(jumpKey))
+        {
+            Debug.Log("Jumping");
+            isJumping = true;
+        }
+        isJumping = false;
     }
 
     private void FlipCharacter()
