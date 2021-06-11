750e8f1a565d87aa906dcf83af5b898902a04e0d
Simar2906 <simar2906@gmail.com>
2021-06-12 02:23:00 +0530

    update player

--- Game Jam/Assets/Scripts/Player_Movement.cs
+++ Game Jam/Assets/Scripts/Player_Movement.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private CharacterController2D player = null;
+    
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private void Awake() 
     {
-        
+        player = GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void MoveLeft()
+    {
+
+    }
+    void SetMoveVector()
+    {
+
     }
 }
