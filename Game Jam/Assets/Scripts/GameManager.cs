2bacb5fa50db77d975e7a8a08aa6c48fbb3f6a28
Simar2906 <simar2906@gmail.com>
2021-06-12 18:56:34 +0530

    pushhhhhh

--- Game Jam/Assets/Scripts/GameManager.cs
+++ Game Jam/Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour{
+public class GameManager : MonoBehaviour
+{
 
     bool gameHasEnded = false;
 
     public int lives = 3;
     public float restartDelay = 1f;
-    public void EndGame ()
+    public GameObject[] darkArray;
+    public GameObject[] lightArray;
+    public bool dark = true;
+
+    public void EndGame()
     {
         if (gameHasEnded == false)
         {
@@ -16,6 +21,55 @@ public class GameManager : MonoBehaviour{
             Invoke("Restart", restartDelay);
             /*Need to add a new if condition on player_movement script to see if the object is falling or colliding with enemy 
             obejct*/
+        }
+    }
+    private void Awake()
+    {
+        darkArray = GameObject.FindGameObjectsWithTag("Dark Platform");
+        lightArray = GameObject.FindGameObjectsWithTag("Light Platform");
+
+    }
+    private void Update()
+    {
+        ThemeChange();
+        Switching();
+    }
+    void Switching()
+    {
+
+        if (Input.GetMouseButtonDown(0)) //left key: 0
+        {
+            dark = !dark;
+        }
+    }
+
+    private void ThemeChange()
+    {
+        if (dark)
+        {
+
+            foreach (GameObject light in lightArray)
+            {
+                Debug.Log(light);
+                light.SetActive(false);
+            }
+            foreach (GameObject dark in darkArray)
+            {
+                dark.SetActive(true);
+            }
+            
+
+        }
+        else
+        {
+            foreach (GameObject light in lightArray)
+            {
+                light.SetActive(true);
+            }
+            foreach (GameObject dark in darkArray)
+            {
+                dark.SetActive(false);
+            }
         }
     }
 
