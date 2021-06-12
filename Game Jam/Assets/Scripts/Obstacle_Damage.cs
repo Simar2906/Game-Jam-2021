88b5470b034c5e81b32c4caa5c58cf672b0226f2
Simar2906 <simar2906@gmail.com>
2021-06-12 15:18:15 +0530

    obstacle script

---
+++ Game Jam/Assets/Scripts/Obstacle_Damage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Damage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player = null;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            player.Damage(1);
+
+        }
+    }
+    private void Damage()
+    {
+        
+    }
+
+}
